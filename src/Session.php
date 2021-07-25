cbae186aab7f610a4f80a3ac27a5a8f891eeff24
Charles <char101@ui.ac.id>
2021-07-25 19:12:34 +0700

    Session: fix session id with ";"

--- src/Session.php
+++ src/Session.php
@@ -2,10 +2,21 @@
 
 class Session extends Workerman\Protocols\Http\Session
 {
+    public function __construct($sessionId)
+    {
+        if ($sessionId[-1] === ';') {
+            $sessionId = substr($sessionId, 0, -1);
+        }
+        parent::__construct($sessionId);
+    }
+
     public static function updateTimestamp($sessionId)
     {
         if (!static::$_handler) {
             static::initHandler();
+        }
+        if ($sessionId[-1] === ';') {
+            $sessionId = substr($sessionId, 0, -1);
         }
         static::$_handler->updateTimestamp($sessionId);
     }
